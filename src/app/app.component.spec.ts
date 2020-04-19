034b432ec9cf6dedb8622699c2becf754db1ffa7
Valery Melou <valery@valerymelou.com>
2020-04-19 22:13:11 +0100

    test: add and fix failling tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,13 @@ import { Component, DebugElement } from '@angular/core';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  template: 'footer'
+})
+class MockFooterComponent {}
 
 @Component({
   selector: 'app-navbar',
@@ -9,18 +16,26 @@ import { AppComponent } from './app.component';
 })
 class MockNavbarComponent {}
 
+@Component({
+  selector: 'app-about-page',
+  template: 'app about'
+})
+class AboutComponent {}
+
 describe('AppComponent', () => {
   let app: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let debugElement: DebugElement;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes([{ path: 'about', component: AboutComponent }])
       ],
       declarations: [
         AppComponent,
+        MockFooterComponent,
         MockNavbarComponent
       ],
     }).compileComponents();
@@ -28,9 +43,21 @@ describe('AppComponent', () => {
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.componentInstance;
     debugElement = fixture.debugElement;
+    router = TestBed.inject(Router);
   }));
 
   it('should create the app', () => {
     expect(app).toBeTruthy();
   });
+
+  it('should not show footer if router url is not /', (async () => {
+    // TODO: is there a better way to test avoid ngZone warning?
+    await fixture.ngZone.run(async () => {
+      app.router.navigate(['/about']);
+    });
+
+    fixture.detectChanges();
+
+    expect(app.showFooter).toBeTrue();
+  }));
 });
