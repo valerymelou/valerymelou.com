ef5284cf3b1d53eb0f4e3a13fc6d84c308c9b5a4
Valery Melou <valery@valerymelou.com>
2020-04-26 21:10:09 +0100

    feat: add the portfolio page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LazyLoadImageModule, scrollPreset } from 'ng-lazyload-image';
 import { SocialLinksComponent } from './social-links/social-links.component';
-
 
 
 @NgModule({
   declarations: [SocialLinksComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LazyLoadImageModule.forRoot({ preset: scrollPreset }),
   ],
-  exports: [SocialLinksComponent]
+  exports: [SocialLinksComponent, LazyLoadImageModule]
 })
 export class SharedModule { }
