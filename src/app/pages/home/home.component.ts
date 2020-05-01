fee26dade1c9cca2345169752b482f223501ec4a
Valery Melou <valery@valerymelou.com>
2020-05-01 11:22:20 +0100

    feat: added SEO meta tags and updated them for each page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SeoService } from 'src/app/core/seo.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
-  title = 'Hi, I\'m Valery!';
-  strap = 'I am a full-stack web developer from Yaounde, Cameroon. I enjoy using web technologies to build everything from small websites to highly interactive apps for web, mobile and desktop.';
+export class HomeComponent {
+  public title = 'Hi, I\'m Valery!';
+  strap = 'I am a web developer from Yaounde, Cameroon. I enjoy using web technologies to build everything from small websites to highly interactive apps for web, mobile and desktop.';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(seoService: SeoService) {
+    seoService.setSocialMediaTags(
+      '',
+      'Web developer from Yaounde, Cameroon',
+      'I am a web developer from Yaounde, Cameroon. I enjoy using web technologies to build everything from small websites to highly interactive apps for web, mobile and desktop.',
+    );
   }
-
 }
