3494c2cd85fa06fcc15a294961cb6d2ee3acd151
Valery Melou <valery@valerymelou.com>
2020-05-02 20:40:06 +0100

    test: add tests for the blog module

--- src/app/shared/pipes/slugify.pipe.spec.ts
+++ src/app/shared/pipes/slugify.pipe.spec.ts
@@ -3,6 +3,6 @@ import { SlugifyPipe } from './slugify.pipe';
 describe('SlugifyPipe', () => {
   it('create an instance', () => {
     const pipe = new SlugifyPipe();
-    expect(pipe).toBeTruthy();
+    expect(pipe.transform('Hello World')).toBe('hello-world');
   });
 });
