031852e93199a4d3ffd8ed3fd84d12920a1aa73e
Valery Melou <me@valerymelou.com>
2022-01-02 15:54:06 +0100

    feat: add blog capabilities with @scullyio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layout/blog-layout/blog-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-layout',
+  templateUrl: './blog-layout.component.html'
+})
+export class BlogLayoutComponent {
+  constructor() { }
+}
