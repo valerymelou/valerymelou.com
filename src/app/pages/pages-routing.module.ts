40cd0241979bc5374418e13ce6323bb5e8ef9316
Valery Melou <me@valerymelou.com>
2022-02-19 19:36:43 +0100

    feat: add about page

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BaseLayoutComponent } from '../layout/base-layout/base-layout.component';
+import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: '',
         component: HomeComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
       }
     ]
   }
