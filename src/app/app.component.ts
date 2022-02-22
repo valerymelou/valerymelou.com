129674d967f0bedc57b5fd6cbd531fa758b9fe4e
Valery Melou <me@valerymelou.com>
2022-02-22 21:06:49 +0100

    wip: add content full

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContentfulService } from './core/contentful.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  constructor(private contentfulService: ContentfulService) {}
+
+  ngOnInit(): void {
+    this.contentfulService.getProjects().subscribe(projects => {
+      console.log(projects);
+    });
+  }
+}
