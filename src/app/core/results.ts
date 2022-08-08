4728351e065ef16da917e22106f29aafb6b1d452
Valery Melou <me@valerymelou.com>
2022-08-08 18:43:04 +0100

    chore: init for v3

--- src/app/core/results.ts
+++
@@ -1,6 +0,0 @@
-export interface Results<T> {
-  items: T[];
-  total: number;
-  skip: number;
-  limit: number;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  contentfulSpaceId: 'gq9ultyr0moo',
-  contentfulAccessToken: '_THg-Rah4LMmdB5RbrL9wu6mchHlkXqvCiGFFtM8dao',
-  contentfulEnvironment: 'master',
-  contentfulRootUrl: 'https://cdn.contentful.com/spaces/',
-  contentfulProjectContentTypeId: 'project',
-  contentfulArticleContentTypeId: 'blogPost'
-};
