557d7911979d3b769a6b5371e15c7445f3502a61
Valery Melou <me@valerymelou.com>
2022-10-04 20:54:06 +0100

    fix: lint issue

--- apps/portfolio/src/app/layout/footer/footer.component.ts
+++ apps/portfolio/src/app/layout/footer/footer.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'vm-footer',
   templateUrl: './footer.component.html',
 })
-export class FooterComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class FooterComponent {}
