e5d9f2410f8e33e06c8cf4ac5d3eb50f97a4ca7f
Valery Melou <valery.melou@mboadigital.tech>
2023-07-12 03:30:59 +0100

    feat: first version of the contentful client

--- apps/blog/src/app/app.config.ts
+++ apps/blog/src/app/app.config.ts
@@ -1,5 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
+import {
+  CONTENTFUL_ACCESS_TOKEN,
+  CONTENTFUL_SPACE,
+} from '@vmelou/contentful/client';
 
 export const appConfig: ApplicationConfig = {
-  providers: [],
+  providers: [
+    {
+      provide: CONTENTFUL_SPACE,
+      useValue: process.env['NX_CONTENTFUL_SPACE'],
+    },
+    {
+      provide: CONTENTFUL_ACCESS_TOKEN,
+      useValue: process.env['NX_CONTENTFUL_ACCESS_TOKEN'],
+    },
+  ],
 };
---
+++ libs/contentful/client/README.md
@@ -0,0 +1,7 @@
+# contentful-client
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test contentful-client` to execute the unit tests.
