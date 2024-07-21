9c8994c37aae6118eb14d2e1b911beaa471d0847
Valery Melou <me@valerymelou.com>
2024-07-21 05:05:14 +0100

    tests: fix failing tests

--- libs/shared/layout/src/test-setup.ts
+++ libs/shared/layout/src/test-setup.ts
@@ -1,8 +1,22 @@
 // @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
 globalThis.ngJest = {
-testEnvironmentOptions: {
-  errorOnUnknownElements: true,
-  errorOnUnknownProperties: true,
-},
+  testEnvironmentOptions: {
+    errorOnUnknownElements: true,
+    errorOnUnknownProperties: true,
+  },
 };
 import 'jest-preset-angular/setup-jest';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
